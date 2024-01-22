e65b60d65661f2a2b7487c9835e30865ff30356a
Beau Pomeroy <beaupomeroy@gmail.com>
2024-01-21 22:11:00 -0700

    finished arrays.js

--- arrays.js
+++ arrays.js
@@ -5,11 +5,15 @@ const faveColors = ['red', 'green', 'black']
 
 //CODE HERE
 
+let colorCopy = faveColors.slice(0,3)
+console.log(colorCopy)
+
 //////////////////PROBLEM 2////////////////////
 /* Add a fourth color to the end of the 'colorCopy' array using the push method. */
 
 //CODE HERE
-
+colorCopy.push('blue')
+console.log(colorCopy)
 //////////////////PROBLEM 3////////////////////
 /*
   Using an array method, create a new array called 'middleNums' from the 
@@ -20,6 +24,8 @@ const numbers = [1, 2, 3, 4, 5]
 
 //CODE HERE
 
+let middleNums = numbers.splice(1,3)
+console.log(middleNums)
 
 //////////////////PROBLEM 4////////////////////
 /* 
@@ -34,3 +40,15 @@ const numbers = [1, 2, 3, 4, 5]
 const bigOrSmallArray = [1, 99, 42, 69, 102, -10, 159, 352]
 
 // CODE HERE
+
+let answers = [];
+
+for (let i = 0; i < bigOrSmallArray.length; i++) {
+  if (bigOrSmallArray[i] > 100) {
+    answers.push('big')
+  }
+  else if (bigOrSmallArray[i] <= 100) {
+    answers.push('small')
+  }
+}
+console.log(answers)
